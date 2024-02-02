59862f776a09e7aaecf05eee37b9cd1bedf9afb2
kitety <aizaizuori@foxmail.com>
2024-02-03 00:38:42 +0800

    feat: reward img

--- themes/hexo/components/RewardButton.js
+++ themes/hexo/components/RewardButton.js
@@ -16,16 +16,37 @@ const RewardButton = () => {
   }
 
   return (
-    <div className='justify-center mt-6'>
-      <div onClick={() => { setShow(value => !value) }}
-        className='bg-[#24a8f0] py-2 w-24 text-center mx-auto animate__jello text-white hover:bg-blue-400 duration-200 transform hover:scale-110 px-3 rounded cursor-pointer'>
-        <i className='mr-2 fas fa-qrcode' />
+    <div className="justify-center mt-6">
+      <div
+        onClick={() => {
+          setShow(value => !value)
+        }}
+        className="bg-[#24a8f0] py-2 w-24 text-center mx-auto animate__jello text-white hover:bg-blue-400 duration-200 transform hover:scale-110 px-3 rounded cursor-pointer"
+      >
+        <i className="mr-2 fas fa-qrcode" />
         <span>打赏</span>
       </div>
 
-      <div id='reward-qrcode' className={`flex space-x-10 my-5 px-5 mx-auto py-6 justify-center bg-white dark:bg-black dark:text-gray-200 ${show ? '' : 'hidden'}`}>
-        <div ><img width='150' height='150' src='https://pomf2.lain.la/f/u8ujbnq.png' /></div>
-        <div ><img width='150' height='150' src='https://pomf2.lain.la/f/8d1edv4o.png' /></div>
+      <div
+        id="reward-qrcode"
+        className={`flex space-x-10 my-5 px-5 mx-auto py-6 justify-center bg-white dark:bg-black dark:text-gray-200 ${
+          show ? '' : 'hidden'
+        }`}
+      >
+        <div>
+          <img
+            width="150"
+            height="150"
+            src="https://bafkreiexusdeuujnkrfdqtszww7qxoe2i3gdt25rhrpxk4e7viw7fsrlxe.ipfs.nftstorage.link/"
+          />
+        </div>
+        <div>
+          <img
+            width="150"
+            height="150"
+            src="https://bafkreihrhyoc3o4fp5oe4ytipeloyussaeep7bpmcgppbw43crs73xepda.ipfs.nftstorage.link/"
+          />
+        </div>
       </div>
     </div>
   )
