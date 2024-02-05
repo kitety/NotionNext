a5c93f424344730b26ab3c54bb8d839747cee3ee
kitety <aizaizuori@foxmail.com>
2024-02-05 22:05:15 +0800

    feat: rebuild

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -3,7 +3,7 @@ import { getServerSideSitemap } from 'next-sitemap'
 import { getGlobalData } from '@/lib/notion/getNotionData'
 import BLOG from '@/blog.config'
 
-export const getServerSideProps = async (ctx) => {
+export const getServerSideProps = async ctx => {
   const { allPages } = await getGlobalData({ from: 'rss' })
   const defaultFields = [
     {
@@ -11,42 +11,51 @@ export const getServerSideProps = async (ctx) => {
       lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
       priority: '0.7'
-    }, {
+    },
+    {
       loc: `${BLOG.LINK}/archive`,
       lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
       priority: '0.7'
-    }, {
+    },
+    {
       loc: `${BLOG.LINK}/category`,
       lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
       priority: '0.7'
-    }, {
+    },
+    {
       loc: `${BLOG.LINK}/feed`,
       lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
       priority: '0.7'
-    }, {
+    },
+    {
       loc: `${BLOG.LINK}/search`,
       lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
       priority: '0.7'
-    }, {
+    },
+    {
       loc: `${BLOG.LINK}/tag`,
       lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
       priority: '0.7'
     }
   ]
-  const postFields = allPages?.filter(p => p.status === BLOG.NOTION_PROPERTY_NAME.status_publish)?.map(post => {
-    const slugWithoutLeadingSlash = post?.slug.startsWith('/') ? post?.slug?.slice(1) : post.slug
-    return {
-      loc: `${BLOG.LINK}/${slugWithoutLeadingSlash}`,
-      lastmod: new Date(post?.publishDay).toISOString().split('T')[0],
-      changefreq: 'daily',
-      priority: '0.7'
-    }
-  })
+  const postFields = allPages
+    ?.filter(p => p.status === BLOG.NOTION_PROPERTY_NAME.status_publish)
+    ?.map(post => {
+      const slugWithoutLeadingSlash = post?.slug.startsWith('/')
+        ? post?.slug?.slice(1)
+        : post.slug
+      return {
+        loc: `${BLOG.LINK}/${slugWithoutLeadingSlash}`,
+        lastmod: new Date(post?.publishDay).toISOString().split('T')[0],
+        changefreq: 'daily',
+        priority: '0.7'
+      }
+    })
   const fields = defaultFields.concat(postFields)
 
   // 缓存
@@ -54,8 +63,9 @@ export const getServerSideProps = async (ctx) => {
     'Cache-Control',
     'public, max-age=3600, stale-while-revalidate=59'
   )
+  console.log('getServerSideSitemap')
 
   return getServerSideSitemap(ctx, fields)
 }
 
-export default () => { }
+export default () => {}
